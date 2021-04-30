88e9990ed5ecb6e51d91eda5620e8c3e79b4ef83
anujmore137 <moreanuj1307@gmail.com>
2021-04-30 23:11:08 +0530

    Update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,41 @@
 <template>
+
+  
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Movie App</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="App.vue">Home</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <table class="table">
       <thead>
         <th>Name</th>
         <th>Director</th>
         <th>Genere</th>
         <th>Rating</th>
-        <th>Release Datess</th>
+        <th>Release Date</th>
       </thead>
       <tbody>
-   
-        <tr v-for="movie in movies" v-bind:key="movie.id" >
+        <tr v-for="movie in movies" v-bind:key="movie.id">
           <td>{{ movie.name }}</td>
           <td>{{ movie.director }}</td>
           <td>{{ movie.genere }}</td>
@@ -31,10 +56,10 @@ export default {
       movies: [],
     };
   },
-  async created(){
-    var response = await fetch('http://127.0.0.1:8000/api/movies/')
+  async created() {
+    var response = await fetch("http://127.0.0.1:8000/api/movies/");
     this.movies = await response.json();
-  }
+  },
 };
 </script>
 
@@ -45,6 +70,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
