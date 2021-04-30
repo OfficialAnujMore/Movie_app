868590c642f7989c836d6e1ded01861401db2567
anujmore137 <moreanuj1307@gmail.com>
2021-04-30 23:28:56 +0530

    Backend connected

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-
   
+
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link to="App.vue">Home</router-link>
+              <router-link to="">Home</router-link>
             </li>
           </ul>
         </div>
